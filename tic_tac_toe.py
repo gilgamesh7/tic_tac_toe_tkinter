0b8a7d7995fbf41a3bec19b1d953eeebbd27695f
gilgamesh7 <felidaesrule@yahoo.com>
2022-07-17 16:38:04 +0530

    Menu & gameboard implemented

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,11 +7,12 @@ from tkinter import font
 from itertools import cycle
 
 class TicTacToeBoard(tk.Tk):
-    def __init__(self):
+    def __init__(self, game):
         super().__init__()
         self.title("Tic Tac Toe")
         self._cells = {}
         self._game = game
+        self._create_menu()
         self._create_board_display()
         self._create_board_grid()
 
@@ -42,6 +43,7 @@ class TicTacToeBoard(tk.Tk):
                     highlightbackground = "lightblue"
                 )
                 self._cells[button] = (row, col)
+                button.bind("<ButtonPress-1>", self.play)
                 button.grid(
                     row=row,
                     column=col,
@@ -49,6 +51,63 @@ class TicTacToeBoard(tk.Tk):
                     pady=5,
                     sticky="nsew"
           )
+    
+    def play(self, event):
+        clicked_btn = event.widget
+        row, col = self._cells[clicked_btn]
+        move = Move(row, col, self._game.current_player.label)
+        if self._game.is_valid_move(move):
+            self._update_button(clicked_btn)
+            self._game.process_move(move)
+            if self._game.is_tied():
+                self._update_display(msg="Tied game!", color="red")
+            elif self._game.has_winner():
+                self._highlight_cells()
+                msg = f'Player "{self._game.current_player.label}" won!'
+                color = self._game.current_player.color
+                self._update_display(msg, color)
+            else:
+                self._game.toggle_player()
+                msg = f"{self._game.current_player.label}'s turn"
+                self._update_display(msg)
+
+    def _update_button(self, clicked_btn):
+        clicked_btn.config(text=self._game.current_player.label)
+        clicked_btn.config(fg=self._game.current_player.color)
+
+    def _update_display(self, msg, color="black"):
+        self.display["text"] = msg
+        self.display["fg"] = color
+
+    def _highlight_cells(self):
+        for button, coordinates in self._cells.items():
+            if coordinates in self._game.winner_combo:
+                button.config(highlightbackground="red")
+
+    def reset_board(self):
+        self._game.reset_game()
+        self._update_display(msg="Ready?")
+        for button in self._cells.keys():
+            button.config(highlightbackground="lightblue")
+            button.config(text="")
+            button.config(fg="black")
+
+    def _create_menu(self):
+        menu_bar = tk.Menu(master=self)
+        self.config(menu=menu_bar)
+        file_menu = tk.Menu(master=menu_bar)
+        file_menu.add_command(
+            label="Play Again",
+            command=self.reset_board
+        )
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=quit)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+
+
+
+
+
 
 @dataclass
 class Player:
@@ -81,7 +140,7 @@ class TicTacToeGame:
     def _setup_board(self):
         self._current_moves = [
             [Move(row,col) for col in range(self.board_size)] 
-            for row in range(self._board_size)
+            for row in range(self.board_size)
         ]
         self._winning_combos = self._get_winning_combos()
 
@@ -129,8 +188,16 @@ class TicTacToeGame:
     def toggle_player(self):
         self.current_player = next(self._players)
 
+    def reset_game(self):
+        for row, row_content in enumerate(self._current_moves):
+            for col, _ in enumerate(row_content):
+                row_content[col] = Move(row, col)
+        self._has_winner = False
+        self.winner_combo = []
+
 def main():
-    board = TicTacToeBoard()
+    game = TicTacToeGame()
+    board = TicTacToeBoard(game)
     board.mainloop()
 
 if __name__ == "__main__":
